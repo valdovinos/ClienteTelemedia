b611e81a29cc11f8a942c49ba3b267c6916b51cc
developer <developer@localhost.localdomain>
2012-12-17 17:02:00 -0600

    version funcional pendiente (en comentarios) de agregar el mono de bienvenida por problrmas en el grid

--- ClienteTelemedia/ClienteTelemedia/src/com/wissen/telemedia/gui/views/WelcomeScreen.java
+++ ClienteTelemedia/ClienteTelemedia/src/com/wissen/telemedia/gui/views/WelcomeScreen.java
@@ -1,21 +1,29 @@
 package com.wissen.telemedia.gui.views;
 
+import imagene.panel.JPanelConFondo;
+
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.wissen.telemedia.gui.ParagraphLabel;
 import com.wissen.telemedia.gui.UIViewListener;
+import com.wissen.telemedia.gui.views.RetrievalStepScreen.ImageSet;
 
 public class WelcomeScreen extends UIView {
 	JPanel panel = new JPanel();
+	
+	ImageIcon icon = new ImageIcon("assets/banner.png");	
 	public WelcomeScreen(UIViewListener listener) {
 		super(listener);
 		
@@ -29,13 +37,33 @@ public class WelcomeScreen extends UIView {
 		c.gridwidth = 4;
 		c.gridheight = 1;
 		
-		addHeading("Bienvenido, " + listener.getSession().firstname);
-
+		//addHeading("Bienvenido, " + listener.getSession().firstname);
+		JPanelConFondo heading = new JPanelConFondo();			
+		JLabel title = new JLabel();		
+		Font font= new Font("newFon", Font.BOLD,
+				(int) (15));
+		Color color = new Color(255, 255, 255);
+		title.setForeground(color);
+		title.setFont(font);
+		title.setPreferredSize(new Dimension(500, 54));
+		title.setText("Bienvenido, " + listener.getSession().firstname);
+		title.setVerticalAlignment(JLabel.CENTER);
+		title.setHorizontalAlignment(JLabel.CENTER);
+		heading.setImagen("assets/banner.png");
+		heading.setPreferredSize(new Dimension(500, 54));
+		heading.add(title);
+		c.gridy++;
+		add(heading, c);
+		JPanelConFondo content = new JPanelConFondo();	
+		content.setImagen("assets/cuadro_blanco.png");
+		content.setPreferredSize(new Dimension(500, 200));
 		JLabel text = new ParagraphLabel("¿Está listo para iniciar el proceso de medición de signos vitales?");
 		
-		c.gridy++;
-		add(text, c);
-		
+		//c.gridy++;
+		c.gridy=0;
+		content.add(text,c);
+		//add(text, c);
+		//add(content,c);
 		JButton continueB = new JButton("Continuar");
 		JButton cancelB = new JButton("Cancelar");
 
@@ -77,13 +105,26 @@ public class WelcomeScreen extends UIView {
 		
 		c.anchor = GridBagConstraints.CENTER;
 
-		add(cancelB, c);
-
+		c.gridy=2;
+		content.add(cancelB, c);
+		c.gridx=1;
+		content.add(continueB, c);
+		c.gridx=0;
+		c.gridy=2;
+		add(content,c);
+		/*JPanelConFondo imageMono = new JPanelConFondo();	
+		imageMono.setPreferredSize(new Dimension(100,250));
+		imageMono.setImagen("assets/mono.png");
+		c.gridx=4;
+		c.gridy=0;
+		add(imageMono, c);
+		//add(cancelB, c);
+*/
 		c.gridx = 2;
 		c.anchor = GridBagConstraints.CENTER;
 		//c.anchor = GridBagConstraints.NORTH;
 
-		add(continueB, c);
+		//add(continueB, c);
 	}
 
 	
