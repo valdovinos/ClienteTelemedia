1dff24f8fbafc20293d5fb93a235598278efb4f0
cabina <cabina@iwm.iwm.org>
2013-02-21 11:28:50 -0600

    implementacion opentok chrome vista VideoConferencia

--- ClienteTelemedia/ClienteTelemedia/src/com/wissen/telemedia/gui/MainWindow.java
+++ ClienteTelemedia/ClienteTelemedia/src/com/wissen/telemedia/gui/MainWindow.java
@@ -10,6 +10,7 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
@@ -214,7 +215,7 @@ public class MainWindow extends JFrame implements UIViewListener {
 				.getLocalGraphicsEnvironment();
 		GraphicsDevice screen = graphicsEnvironment.getDefaultScreenDevice();
 		screen.setFullScreenWindow(frame);
-
+		
 	}
 
 	/**
